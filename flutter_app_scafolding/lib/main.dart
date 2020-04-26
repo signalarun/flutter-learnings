704492638bb14a494814997b45210a4223bdd166
signalarun <signalarun@gmail.com>
2020-04-27 02:29:27 +0530

    Update : appbar color

--- flutter_app_scafolding/lib/main.dart
+++ flutter_app_scafolding/lib/main.dart
@@ -5,10 +5,11 @@ void main() {
     home: Scaffold(
         backgroundColor: Colors.white70,
         appBar: AppBar(
+          backgroundColor: Colors.pink,
           title: Text("My first App"),
         ),
 
-        body: Image(image: NetworkImage('http://theaposition.com/robertfagan/wp-content/uploads/sites/33/2012/04/happy.jpg')),
+        body: Center(child: Image(image: NetworkImage('http://theaposition.com/robertfagan/wp-content/uploads/sites/33/2012/04/happy.jpg'))),
 
     ),
     ),
