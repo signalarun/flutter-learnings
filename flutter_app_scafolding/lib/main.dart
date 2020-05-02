dad5577780b6b253956adb833d26a3b333ef0f17
signalarun <signalarun@gmail.com>
2020-05-03 01:49:43 +0530

    Hot reloading

--- flutter_app_scafolding/lib/main.dart
+++ flutter_app_scafolding/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
+  runApp(MyApp());
+  /*
   runApp(MaterialApp(
     home: Scaffold(
         backgroundColor: Colors.white70,
@@ -14,4 +17,25 @@ void main() {
     ),
     ),
   );
+    */
+}
+
+/// Allows hot reloading
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.white70,
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+            title: Text("My first App"),
+          ),
+
+          body: Center(child: Image(image: NetworkImage('http://theaposition.com/robertfagan/wp-content/uploads/sites/33/2012/04/happy.jpg'))),
+
+        )
+    );
+  }
+
 }
