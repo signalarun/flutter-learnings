76a9f673ef77b127c3efdf68a77e7ed80608779a
Arun Gopi <signalarun@gmail.com>
2021-02-12 12:03:08 +0530

    Update functions.dart

--- dart-samples/functions.dart
+++ dart-samples/functions.dart
@@ -1,13 +1,22 @@
 void main() {
-  greetings1( "How are you ","Gopi");
-  greetings2( greetings:'how are you ', name:'Arun', );
+  greetings1("How are you ", "Gopi");
+  greetings2(
+    greetings: 'how are you ',
+    name: 'Arun',
+  );
   
+  print(getGreeting());
 }
 
-void greetings1(String greetings, String name){
- print("$greetings $name");
+void greetings1(String greetings, String name) {
+  print("$greetings $name");
 }
 
-void greetings2({String name, String greetings}){
- print("$greetings $name ");
+void greetings2({String name, String greetings}) {
+  print("$greetings $name ");
 }
+
+String getGreeting(){
+  return "Hello";
+}
+
