4655d07f66f146533a35f56bae3d4de829ca95ea
Arun Gopi <signalarun@gmail.com>
2022-01-27 01:24:55 +0530

    Create AbstractClassDemo.dart

---
+++ dart-samples/class/AbstractClassDemo.dart
@@ -0,0 +1,10 @@
+// Demo of abstract class
+void main() {
+}
+// abstract Vehicle class declaration
+abstract class Vehicle{
+  // abstract methods
+  String getVehicleDetail();
+  stop();
+  start();  
+}
